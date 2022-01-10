9448d809e7bf7aa2d3492e8e0066254fdf23e2d9
Glitch (glitch-blank-website) <none>
2022-01-10 07:51:30 +0000

    🎄👀 Checkpoint ./script.js:27704817/961 ./changelog.html:27704817/95 ./style.css:27704817/64

--- script.js
+++ script.js
@@ -7,17 +7,11 @@ const ta = (() => {
     if (b.measureText) {
         if (b.measureText("test").emHeightAscent)
             return (a, c, d = !1) => {
-            c += B.graphical.fontSizeBoost;
+            c += b.graphical.fontSizeBoost;
             b.font = "bold " + c + "px Ubuntu";
             a = b.measureText(a);
-            return d
-            ? {
-            width: a.width,
-            height: a.emHeightAscent
-            }
-            : a.width;
-            }
-    }
+            return d ? {width: a.width, height: a.emHeightAscent} : a.width;
+            };
     let a = document.createElement("div");
     a.style.padding = "0";
     a.style.margin = "0";
@@ -25,12 +19,24 @@ const ta = (() => {
     a.style.visibility = "hidden";
     document.body.appendChild(a);
     return (c, d, e = !1) => {
-    d += B.graphical.fontSizeBoost;
+    d += b.graphical.fontSizeBoost;
     if (e)
         return ((a.style.font = "bold " + d + "px Ubuntu"),
         (a.innerText = c), { width: a.clientWidth, height: a.clientHeight }
         ); b.font = "bold " + d + "px Ubuntu";
         return b.measureText(c).width;
     };
-          
-}
+    };
+    let a = document.createElement("div");
+    a.style.padding = "0";
+    a.style.margin = "0";
+    a.style.position = "absolute";
+    a.style.visibility = "hidden";
+    a.style.whiteSpace = "pre";
+    document.body.appendChild(a);
+    return (b, d, e = !1) => {
+    d += b.graphical.fontSizeBoost;
+    a.style.font = "bold " + d + "px Ubuntu";
+    return e ? {width: a.clientWidth, height: a.clientHeight} : a.clientWidth;
+    }
+ })();     
