a8e7f2dc518c31de9b977107732258a6411c7df4
Glitch (glitch-blank-website) <none>
2022-01-10 07:39:59 +0000

    🕴📮 Checkpoint ./style.css:27704817/54 ./script.js:27704817/1691

--- script.js
+++ script.js
@@ -2,3 +2,35 @@
     to your site with Javascript */
 
 console.log("Hello, world!");
+const ta = (() => {
+    let b = document.createElement("canvas").getContext("2d");
+    if (b.measureText) {
+        if (b.measureText("test").emHeightAscent)
+            return (a, c, d = !1) => {
+            c += B.graphical.fontSizeBoost;
+            b.font = "bold " + c + "px Ubuntu";
+            a = b.measureText(a);
+            return d
+            ? {
+            width: a.width,
+            height: a.emHeightAscent
+            }
+            : a.width;
+            }
+    }
+    let a = document.createElement("div");
+    a.style.padding = "0";
+    a.style.margin = "0";
+    a.style.position = "absolute";
+    a.style.visibility = "hidden";
+    document.body.appendChild(a);
+    return (c, d, e = !1) => {
+    d += B.graphical.fontSizeBoost;
+    if (e)
+        return ((a.style.font = "bold " + d + "px Ubuntu"),
+        (a.innerText = c), { width: a.clientWidth, height: a.clientHeight }
+        ); b.font = "bold " + d + "px Ubuntu";
+        return b.measureText(c).width;
+    };
+          
+}
